61ec398df29ceb61fd544bf68e8d1d92b1d68d4d
Dmitriy <dkaukin@gmail.com>
2018-07-31 15:26:10 +0600

    add homework to day2

--- master/day2/examples/react_day2/src/App.js
+++ master/day2/examples/react_day2/src/App.js
@@ -1,22 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoDone from "./Todo/TodoDone";
+import TodoList from "./Todo/TodoList";
+
+
+let id = 0;
+
+function getTodoId() {
+  id += 1;
+  return id;
+}
 
 class App extends Component {
+  state = {
+    inputValue: "",
+    todos: [
+      {
+        id: 0,
+        value: "Сходить за продуктами",
+        done: false
+      }
+    ]
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ inputValue: value });
+  };
+
+  handleKeyDown = event => {
+    if (event.keyCode === 13) {
+      const { inputValue, todos } = this.state;
+      const newTodo = { value: inputValue, done: false, id: getTodoId() };
+
+      this.setState({ inputValue: "", todos: [...todos, newTodo] });
+    }
+  };
+
+  cahngeDoneState = (id, val) => {
+
+    this.setState()
+
+  };
+
+  handleClickCheckbox = id => {
+    this.setState(state => ({
+      todos: state.todos.map(
+        todo => (id === todo.id ? { ...todo, done: !todo.done } : todo)
+      )
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(state => ({
+      todos: state.todos.filter(todo => id !== todo.id)
+    }));
+  };
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { inputValue, todos } = this.state;
+    const todos_ = todos.filter(el => !el.done);
+    const todo_done = todos.filter(el => el.done);
+    return <div className="container">
+        <div className="row">
+          <div className="left col-sm-6">
+            <div className="input-group col-sm-12">
+              <input type="text" className="form-control" placeholder="Введите ваш план" value={inputValue} onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
+            </div>
+            <div className="col-sm-10">
+              {todos_.map(todo => (
+                <TodoList
+                  onChange={this.handleClickCheckbox}
+                  onDelete={this.handleDelete}
+                  key={todo.id}
+                  id={todo.id}
+                  text={todo.value}
+                  done={todo.done}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="right col-sm-6">
+            <div>
+              {todo_done.map(todo => (
+                <TodoDone
+                  onChange={this.handleClickCheckbox}
+                  key={todo.id}
+                  id={todo.id}
+                  text={todo.value}
+                  done={todo.done}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>;
   }
 }
+
 
 export default App;
