2cf44e1ff2e161541a2c3ba55f182420e1476bbc
galym <almabek.galym@gmail.com>
2018-08-01 13:12:13 +0600

    Second day homework - done

--- galym/day_2/homework/comment-app/src/CommentList/CommentList.jsx
+++ galym/day_2/homework/comment-app/src/CommentList/CommentList.jsx
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './CommentList.css';
 
 class CommentList extends Component {
+
+	handleDelete = () => {
+		const { id, name, onDelete } = this.props;
+		onDelete(id, name);
+	}
 	
   render() {
-		console.log(window.location.search);
+		const { id, name, comment } = this.props;
     return (
       <div className="CommentList">
-        <div id="write">
-
-				</div>
+				<ul className="list-group">
+					<li className="list-group-item" key={id}>
+						<h3>{name}</h3>
+						<p>{comment}</p>
+						<button 
+							type="button" 
+							className="btn btn-danger"
+							onClick={this.handleDelete}
+						>Delete</button>
+					</li>
+				</ul>
       </div>
     );
   }
