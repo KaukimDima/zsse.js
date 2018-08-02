ee8a3e5294c87827a663bea2a25c2840d415c402
Dmitriy <dkaukin@gmail.com>
2018-08-02 12:17:21 +0600

    fix work

--- master/day3/examples/react_lifecycle/src/App.js
+++ master/day3/examples/react_lifecycle/src/App.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import TodoShow from "./ToDo/TodoShow";
+import React, { Component } from "react";
+import "./App.css";
+import FetchTodoList from "./FetchTodoList/FetchTodoList";
+
 class App extends Component {
   state = {
     inputs: 1,
-    todoso_id: [1, 5, 9],
-    result : []
+    value: [],
+    result: []
   };
 
-  componentDidMount() {
-    this.state.todoso_id.forEach( (v, i, arr) =>  {
-      fetch(`https://jsonplaceholder.typicode.com/todos/${v}`).
-        then( data => data.json() ).
-        then( json => this.setState({
-          result : [...this.state.result, json]
-        }));
+  handleAddEvent = e => {
+    let { inputs } = this.state;
+    inputs += 1;
+    this.setState({ inputs: inputs });
+  };
+
+  // handleFetchEvent = e => {
+  //   const input = document.getElementsByTagName("input");
+  //   let arr = [];
+  //   for (let i = 0; i < input.length; i++) {
+  //     arr.push(+input[i].value);
+  //   }
+  //   this.setState({ value: "jghffdg" });
+  //   console.log(this.state);
+  // };
+
+  handleFetchEvent = e => {
+    const input = document.getElementsByTagName("input");
+    let arr = [];
+    for (let i = 0; i < input.length; i++) {
+      arr.push(+input[i].value);
+    }
+
+    let arr_obj = [];
+
+    arr.forEach(v => {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${v}`)
+        .then(data => data.json())
+        .then(json => arr_obj.push(json) );
+        // this.setState({ result: [...this.state.result, json] })
     });
-  }
+
+    this.setState({ result: arr_obj });
+
+  };
+
 
   newInput = e => {
     e.target.preventDefault;
@@ -28,21 +56,34 @@ class App extends Component {
   render() {
     let arr = [];
     for (let i = 0; i < this.state.inputs; i++) {
-      arr.push(<input data-id={i} key={i} onClick={this.conLog} />);
+      arr.push(
+        <input
+          key={i}
+          className="form-control"
+          placeholder="Type number..."
+          onClick={this.conLog}
+        />
+      );
     }
-    console.log(this.state);
-    return (
-      <div>
-        <form onSubmit action="">
-          <button type="button" onClick={this.newInput}>
-            add
+
+    console.log('render', this.state);
+
+    return <div className="App">
+        <form className="form-group" id="form">
+          <button type="button" className="btn btn-primary" onClick={this.handleFetchEvent}>
+            Fetch
           </button>
-          {arr}
-          <button type="submit">submit</button>
+          <button type="button" id="button" className="btn btn-success" onClick={this.handleAddEvent}>
+            Add
+          </button>
         </form>
-        <TodoShow result={this.state.result} />
-      </div>
-    );
+
+        <hr />
+        {arr}
+        <hr />
+
+        <FetchTodoList value="this.state.value" />
+      </div>;
   }
 }
 
