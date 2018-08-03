89eec892cf365d29d019bbc2c27d58bb8fd4fdf8
Abay <a.aubakirov@zeinetsse.com>
2018-08-03 10:24:29 +0600

    push

--- ABQ/day4/create_app/src/router/UserList.js
+++ ABQ/day4/create_app/src/router/UserList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, HashRouter, Route, Link } from "react-router-dom";
-
+import EachUser from './EachUser'
 class UserList extends Component { 
 
 state = {
@@ -17,29 +17,38 @@ componentDidMount(){
 
 render(){
     console.log(this.state.users);
-
+    const {users} = this.state;
 return(
-    <Router>
-    <div> 
-    {this.state.users.map(            
-            (el, i) => 
-            <Link to   = "/userList/{el.id}"> 
-            <p key = {i}> {el.email}  </p> 
-            </Link>
+    
+        <Router>
+            <div>
+        <ul>
+    {users.map(            
+            (el) => 
+            
+                <Link to = {`/eachUser/${el.id}`}> 
+                 <p key={el.id}>{el.email}</p>   
+                </Link>          
+    )}
+    </ul>
+    {/* {users.map(            
+            (el) => 
 
-     <Route path="/userList/:id" render = {  }/>
-    )} 
-
-
+     <Route exact path={`/eachUser/${el.id}`} 
+     render={props => <EachUser key = {props.id} someprops={el.id} {...props} />}  */}
+    {/* //  render = {
+    //      () => 
+    //         <h1> ID: {el.id} </h1>
+         
+    //  } 
+    />
+    )} */}
     </div>
     </Router>
-)
-}
+    
+)}
 };
 
-const justFunc = () => {
-
-}
 
 
 export default UserList;
