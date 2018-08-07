382aa5331ff6b7ee913507772c5a5ad87f839841
Dmitriy <dkaukin@gmail.com>
2018-08-07 13:35:30 +0600

    galym - fix problem

--- galym/day_6/classwork/redux/src/App.js
+++ galym/day_6/classwork/redux/src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getComments } from './reducers';
 
-const handleKeyDown = e => {
-	const { addComment } = this.props;
-	const { commentBody } = this.state;
-	if (e.keyCode === 13) {
-		addComment(commentBody);
-		this.setState({ commentBody: '' });
-	}
-}
 
-const handleChange = e => {
-	this.setState({ commentBody: e.target.value });
-}
 
 class App extends Component {
 	
@@ -21,14 +9,27 @@ class App extends Component {
 		commentBody: ''
 	};
 
+	handleKeyDown = e => {
+		const { addComment } = this.props;
+		const { commentBody } = this.state;
+		if (e.keyCode === 13) {
+			addComment(commentBody);
+			this.setState({ commentBody: '' });
+		}
+	}
+
+	handleChange = e => {
+	this.setState({ commentBody: e.target.value });
+	}
+
   render() {
 		const { comments } = this.props;
     return (
       <div className="App">
 				<input
 					value={ this.state.commentBody }
-					onKeyDown={ handleKeyDown }
-					onChange={ handleChange }
+					onKeyDown={ this.handleKeyDown }
+					onChange={ this.handleChange }
 				/>
 				{<div>
 					{ comments.map( (el, i) => <div key={i}><b>{el}</b></div>) }
