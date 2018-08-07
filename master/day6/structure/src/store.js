58ffb1388c747ff0d0586b66ce5292e6588d88af
Dmitriy <dkaukin@gmail.com>
2018-08-07 14:29:47 +0600

    structure of code

---
+++ master/day6/structure/src/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+
+export default initialState =>
+  createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
