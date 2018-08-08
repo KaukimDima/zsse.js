1720c1633cd03d901f724097c77a4fdbe1f1bf25
galym <almabek.galym@gmail.com>
2018-08-08 12:25:25 +0600

    removeItem bug

--- galym/day_5/authentication-app/src/actions/recordsActions.jsx
+++ galym/day_5/authentication-app/src/actions/recordsActions.jsx
@@ -1,6 +1,12 @@
 import { ADD_RECORD } from './recordsTypes';
+import { REMOVE_RECORD } from './recordsTypes';
 
 export const addRecord = payload => ({
 	type: ADD_RECORD,
+	payload
+});
+
+export const removeRecord = payload => ({
+	type: REMOVE_RECORD,
 	payload
 });
