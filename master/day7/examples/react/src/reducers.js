c1ac3617a7a56341d731d83ebe89cf55630a7a92
Dmitriy <dkaukin@gmail.com>
2018-08-10 10:28:36 +0600

    classwork2 day8

--- master/day7/examples/react/src/reducers.js
+++ master/day7/examples/react/src/reducers.js
@@ -1,49 +1,50 @@
 import {
   fetchFireFlyEpisodsRequest,
   fetchFireFlyEpisodsSuccess,
-  fetchFireFlyEpisodsFailure
+  fetchBlogFail
 } from './actions';
 import {combineReducers} from 'redux';
 import {handleAction, handleActions} from 'redux-actions';
 
-const result = handleAction(
+const episodes = handleAction(
   fetchFireFlyEpisodsSuccess,
   (state, action) => action.payload,
   []
 );
 
 const error = handleAction(
-  fetchFireFlyEpisodsFailure,
+  fetchBlogFail,
   (state, action) => action.error,
   null
 );
+
 
 const isFetching = handleActions(
   {
     [fetchFireFlyEpisodsRequest]: () => true,
     [fetchFireFlyEpisodsSuccess]: () => false,
-    [fetchFireFlyEpisodsFailure]: () => false
+    [fetchBlogFail]: () => false
   },
-  false
+  true
 );
 
 const isFetched = handleActions(
   {
     [fetchFireFlyEpisodsRequest]: () => false,
     [fetchFireFlyEpisodsSuccess]: () => true,
-    [fetchFireFlyEpisodsFailure]: () => true
+    [fetchBlogFail]: () => true
   },
   false
 );
 
 export default combineReducers({
   error,
-  result,
+  episodes,
   isFetched,
   isFetching
 });
 
-export const getEpisodes = state => state.result;
+export const getEpisodes = state => state.episodes;
 export const getIsFetching = state => state.isFetching;
 export const getIsFetched = state => state.isFetched;
 export const getError = state => state.error;
