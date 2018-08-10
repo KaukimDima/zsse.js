a89db2030646c66c489b0130a9108c28de39f235
Abay <a.aubakirov@zeinetsse.com>
2018-08-10 10:46:13 +0600

    push

--- ABQ/day9/practice_app/src/index.js
+++ ABQ/day9/practice_app/src/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import createStore from './store';
+import {connect} from 'react-redux';
+import {fetchFireFlyEpisodsRequest} from './actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {
+  getEpisodes,
+  getError,
+  getIsFetching,
+  getIsFetched
+} from './reducers';
+
+const store = createStore();
+
+class App extends Component {
+  componentDidMount() {
+    const {
+      isFetched,
+      fetchFireFlyEpisodsRequest
+    } = this.props;
+
+    if (!isFetched) fetchFireFlyEpisodsRequest();
+  }
+
+  render() {
+    const {isFetching, error, episodes} = this.props;
+    console.log(this.props);
+    if (isFetching) {
+      return <p>Идет загрузка</p>;
+    }
+
+    if (error !== null) {
+      return <p style={{color: 'red'}}>Ошибка! {error}</p>;
+    }
+
+    return (
+      <div>
+        {episodes.map((el, i) => (
+          <div key={i}>
+            <p>{el.text}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  episodes: getEpisodes(state),
+  error: getError(state),
+  isFetching: getIsFetching(state),
+  isFetched: getIsFetched(state)
+});
+
+const mapDispatchToProps = {
+  fetchFireFlyEpisodsRequest
+};
+
+const EnhancedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <EnhancedApp />
+  </Provider>,
+  document.getElementById('root')
+);
